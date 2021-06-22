80295fbf6e312c5c783366d79d38218118cc7f6e
Ullash04 <mhullash57@gmail.com>
2021-06-22 16:14:43 +0600

    Project Forms class work. 3 form done of Station master.

---
+++ MID_PROJECT/Admin message.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <h1>Leave Your Message For Admin
+    </head>
+	
+    <body>
+        <form>
+                <table align="center">
+
+                    <tr>
+                        <td align="right">
+						Admin Message:
+                        </td>
+                        <td>
+                            <textarea> </textarea>
+                        </td>
+					<tr>
+					<td rowspan="2" align ="center"> 
+					 <input type="button" value = "Send" >
+					 </td>
+				    </tr>
+                    </tr>
+                </table>
+        </form>
+    </body>
+</html>
---
+++ MID_PROJECT/Emergency message.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <h1>Emergency Message</h1>
+    </head>
+    <body>
+        <form>
+                <table align="center">
+
+					<tr>
+                        <td align="right">
+                            Emergency Type:
+                        </td>
+                        <td>
+                            <textarea> </textarea>
+                        </td>
+					</tr>
+					<tr>
+					    <td align="right">
+                            To:
+                        </td>
+                        <td>
+                            <textarea> </textarea>
+                        </td>
+                    </tr>
+					<tr>
+					    <td align="right">
+                            Message:
+                        </td>
+                        <td>
+                            <textarea> </textarea>
+                        </td>
+                    </tr>
+					<tr>
+					<td rowspan="2" align ="center"> 
+					 <input type="button" value = "Send" >
+					 </td>
+				    </tr>
+					
+                </table>
+        </form>
+    </body>
+</html>
